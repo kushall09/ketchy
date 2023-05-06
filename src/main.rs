264f9d0902447b3009213334e7f126b0695ce7b1
Anonymous Noble <83326666+kushall09@users.noreply.github.com>
2023-05-06 15:28:16 +0545

    temperature , host and color palette added

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,22 @@ fn main() {
     let username = username();
     let hostname = hostname();
     let os_name= distro();
+    let device=nixinfo::device().unwrap_or_else(|_| "Unavailable".to_string());
+    let temp=nixinfo::temp().unwrap_or_else(|_| "Unavailable".to_string());
     let kernel = nixinfo::kernel().unwrap_or_else(|_| "Unavailable".to_string());
     let uptime = nixinfo::uptime().unwrap_or_else(|_| "Unavailable".to_string());
      let mem = nixinfo::memory().unwrap_or_else(|_| "Unavailable".to_string());
 
     let ascii_art = ascii::AS;
-    println!(" ");
+    println!("  ");
     println!("{}@{}",username.red(),hostname.blue());
     println!(" {}", ascii_art.green());
     println!("   ");
-    println!("OS: {}", os_name.purple());
+    println!("OS: {}", os_name.blue());
+    println!("Host: {}",device.purple());
     println!("Kernel: {}", kernel.cyan());
     println!("Uptime: {}", uptime.yellow());
+    println!("Temp : {}°C",temp.red());
     println!("Memory : {}", mem.white());
+    println!("{} {} {} {} {}","●".red(),"●".yellow(),"●".cyan(),"●".blue(),"●".white());
 }
